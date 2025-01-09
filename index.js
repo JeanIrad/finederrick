a9c44f894f8ddf892d931f8c3b547f80ffe76dd8
JeanIrad <jado.milton@gmail.com>
2025-01-09 11:05:10 +0200

    responsive design

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "YouTube", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+  strings: ["Frontend Developer", "YouTube", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+const menuBar = document.querySelector(".fa-bars");
+
+menuBar.addEventListener("click", () => {
+  const menu = document.querySelector(".navbar");
+  menu.classList.toggle("show-menu");
 });
